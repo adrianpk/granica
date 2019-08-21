99bb186f006d9eba162116c5092e4b481a016c29
Adrian K <adrian.git@kuguar.dev>
2019-08-21 13:32:36 +0200

    Postgres database handler updated.

--- internal/postgres/postgres.go
+++ internal/postgres/postgres.go
@@ -2,6 +2,9 @@ package postgres
 
 import (
 	"context"
+	"fmt"
+	"hash/fnv"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql" // package init.
 	"github.com/jmoiron/sqlx"
@@ -16,31 +19,72 @@ var (
 )
 
 type (
-	// Handler is a DB handler.
+	// DbHandler is a DB handler.
 	DbHandler struct {
 		*svc.BaseHandler
 		Conn *sqlx.DB
 	}
 )
 
-// InitDb creates and return a new DB handler.
-// it also stores it as the package default handler.
-func InitDb(ctx context.Context, cfg *config.Config, log *log.Logger) (*DbHandler, error) {
-	var err error
-	Db, err = newHandler(ctx, cfg, log)
-	if err != nil {
-		return nil, err
+// NewHandler creates and returns a new DB handler.
+func NewHandler(ctx context.Context, cfg *config.Config, log *log.Logger) (*DbHandler, error) {
+	hn := fmt.Sprintf("postgres-db-handler-%s", nameSufix())
+	log.Info("New handler", "name", hn)
+	h := &DbHandler{
+		BaseHandler: svc.NewBaseHandler(ctx, cfg, log, hn),
 	}
-	return Db, nil
+	return h, nil
 }
 
-// NewHandler creates and returns a new DB handler.
-func newHandler(ctx context.Context, cfg *config.Config, log *log.Logger) (*DbHandler, error) {
-	h := &DbHandler{
-		BaseHandler: svc.NewBaseHandler(ctx, cfg, log, "postgres-db-handler"),
+// Init a new Postgres database handler.
+// it also stores it as the package default handler.
+func (h *DbHandler) Init(s svc.Service) chan bool {
+	// Set package default handler.
+	// TODO: See if this could be avoided.
+	Db = h
+
+	ok := make(chan bool)
+	go func() {
+		defer close(ok)
+		conn, err := h.Connect()
+		if err != nil {
+			s.Log().Error(err, "Init Postgres Db handler error")
+			ok <- false
+			return
+		}
+		h.Conn = conn
+		s.Lock()
+		s.AddHandler(h)
+		s.Unlock()
+		h.Log().Info("Handler initializated", "name", h.Name())
+		ok <- true
+	}()
+	return ok
+}
+
+// Start Postgres database handler.
+func (h *DbHandler) Start() error {
+	h.Enable()
+	return nil
+}
+
+// Connect to Posgres database.
+func (h *DbHandler) Connect() (*sqlx.DB, error) {
+	res := <-h.RetryConnection()
+	if res.err != nil {
+		return nil, res.err
 	}
+	return res.conn, nil
+}
 
-	h.Conn = <-h.RetryConnection()
 
-	return h, nil // TODO: RetryConnection will eventually throw a timeout error.
+func nameSufix() string {
+	digest := hash(time.Now().String())
+	return digest[len(digest)-8:]
+}
+
+func hash(s string) string {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return fmt.Sprintf("%d", h.Sum32())
 }
