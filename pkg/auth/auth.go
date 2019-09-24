526b9073b42c3fdf27761e8edf321a8526bab2ea
Adrian K <adrian.git@kuguar.dev>
2019-09-24 23:06:19 +0200

    Auth worker updated.

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -2,21 +2,41 @@ package auth
 
 import (
 	"context"
+	"net/http"
+
 	"gitlab.com/mikrowezel/config"
 	logger "gitlab.com/mikrowezel/log"
 	svc "gitlab.com/mikrowezel/service"
 )
 
-type Auth struct {
-	*svc.BaseWorker
-}
+type (
+	Auth struct {
+		*svc.BaseWorker
+		Server http.Handler
+	}
 
-// NewWorker creates a new base worker instance.
-// This is a bare implementtion of Worker interface
-// just for mocking and/or testing purposes.
+	AuthCtx struct {
+	}
+
+	contextKey string
+)
+
+const (
+	userCtxKey contextKey = "auth"
+)
+
+// NewWorker creates a new Auth worker instance.
 func NewWorker(ctx context.Context, cfg *config.Config, log *logger.Logger, name string) *Auth {
 	w := &Auth{
 		BaseWorker: svc.NewWorker(ctx, cfg, log, "granica-auth-worker"),
 	}
+	w.AddServer()
 	return w
+}
+
+func (a *Auth) Start() error {
+	p := a.Cfg().ValOrDef("server.port", ":8080")
+	err := http.ListenAndServe(p, a.Server)
+	a.Log().Error(err)
+	return err
 }
