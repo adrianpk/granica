6b403700ad50579b3ab575f4b7248d20110b6bf9
Adrian K <adrian.git@kuguar.dev>
2019-09-26 23:39:33 +0200

    User context key updated.

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -22,7 +22,7 @@ type (
 )
 
 const (
-	userCtxKey contextKey = "auth"
+	userCtxKey contextKey = "user"
 )
 
 // NewWorker creates a new Auth worker instance.
@@ -36,6 +36,7 @@ func NewWorker(ctx context.Context, cfg *config.Config, log *logger.Logger, name
 
 func (a *Auth) Start() error {
 	p := a.Cfg().ValOrDef("server.port", ":8080")
+	a.Log().Info("Server initializing", "port", p)
 	err := http.ListenAndServe(p, a.Server)
 	a.Log().Error(err)
 	return err
