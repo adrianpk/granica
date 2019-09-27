ec8b3d623cf47e029a108fa2c69434da14f551d7
Adrian K <adrian.git@kuguar.dev>
2019-09-27 17:58:13 +0200

    Router refactored.

--- pkg/auth/server.go
+++ pkg/auth/server.go
@@ -17,30 +17,45 @@ func (t textResponse) write(w http.ResponseWriter, r *http.Request) {
 
 // AddServer to worker.
 func (a *Auth) AddServer() http.Handler {
-	rt := chi.NewRouter()
-	rt.Use(middleware.RequestID)
-	rt.Use(middleware.RealIP)
-	rt.Use(middleware.Recoverer)
-	rt.Use(middleware.Timeout(60 * time.Second))
+	// Home
+	// Middlewares
+	hr := chi.NewRouter()
+	hr.Use(middleware.RequestID)
+	hr.Use(middleware.RealIP)
+	hr.Use(middleware.Recoverer)
+	hr.Use(middleware.Timeout(60 * time.Second))
+	a.addHomeRoutes(hr)
 
+	// API
+	ar := a.makeAPIRouter(hr)
+
+	// User
+	a.makeUserAPIRouter(ar)
+
+	a.Server = hr
+
+	return hr
+}
+
+func (a *Auth) addHomeRoutes(rt chi.Router) {
 	rt.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		tr := textResponse("Granica is running!")
 		rt.Get("/", tr.write)
 	})
+}
 
-	apiRt := rt.Route("/api/v1", func(apiRt chi.Router) {
+func (a *Auth) makeAPIRouter(parent chi.Router) chi.Router {
+	return parent.Route("/api/v1", func(apiRt chi.Router) {
 		tr := textResponse("API v1.0")
 		apiRt.Get("/", tr.write)
 	})
+}
 
-	apiRt.Route("/users", func(userRt chi.Router) {
-		userRt.Use(userCtx)
-		userRt.Get("/", a.getUsers)
+func (a *Auth) makeUserAPIRouter(parent chi.Router) chi.Router {
+	return parent.Route("/users", func(userAPIRt chi.Router) {
+		userAPIRt.Use(userCtx)
+		userAPIRt.Get("/", a.getUsers)
 	})
-
-	a.Server = rt
-
-	return rt
 }
 
 func userCtx(next http.Handler) http.Handler {
