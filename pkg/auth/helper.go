fdcd287865058d6f07ce3e9dd05849642bd02a42
Adrian K <adrian.git@kuguar.dev>
2019-10-01 12:03:06 +0200

    Database null values builders moved to db package.

--- pkg/auth/helper.go
+++ pkg/auth/helper.go
@@ -1,17 +1,20 @@
 package auth
 
-import "gitlab.com/mikrowezel/granica/internal/model"
+import (
+	"gitlab.com/mikrowezel/db"
+	"gitlab.com/mikrowezel/granica/internal/model"
+)
 
 // toModel creates a User model fron transport values.
 func (cur *CreateUserReq) toModel() model.User {
 	return model.User{
-		Username:          toNullString(cur.Username),
+		Username:          db.ToNullString(cur.Username),
 		Password:          cur.Password,
-		Email:             toNullString(cur.Email),
-		EmailConfirmation: toNullString(cur.EmailConfirmation),
-		GivenName:         toNullString(cur.GivenName),
-		MiddleNames:       toNullString(cur.MiddleNames),
-		FamilyName:        toNullString(cur.FamilyName),
-		// Geolocation:    toNullGeometry(cur.Lat, cur.Lng)
+		Email:             db.ToNullString(cur.Email),
+		EmailConfirmation: db.ToNullString(cur.EmailConfirmation),
+		GivenName:         db.ToNullString(cur.GivenName),
+		MiddleNames:       db.ToNullString(cur.MiddleNames),
+		FamilyName:        db.ToNullString(cur.FamilyName),
+		// Geolocation:    db.ToNullGeometry(cur.Lat, cur.Lng)
 	}
 }
