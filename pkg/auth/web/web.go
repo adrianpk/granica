817396892f77bc6fa5596348e6e93e336fbe8f13
Adrian K <adrian.git@kuguar.dev>
2019-11-21 01:57:49 +0100

    Service field.

--- pkg/auth/web/web.go
+++ pkg/auth/web/web.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"gitlab.com/mikrowezel/backend/config"
-	"gitlab.com/mikrowezel/backend/granica/pkg/auth/service"
+	svc "gitlab.com/mikrowezel/backend/granica/pkg/auth/service"
 	"gitlab.com/mikrowezel/backend/log"
 	"gitlab.com/mikrowezel/backend/web"
 )
@@ -12,16 +12,18 @@ import (
 type (
 	Endpoint struct {
 		*web.Endpoint
+		service *svc.Service
 	}
 )
 
-func MakeEndpoint(ctx context.Context, cfg *config.Config, log *log.Logger, service *service.Service) (*Endpoint, error) {
-	wep, err := web.MakeEndpoint(ctx, cfg, log, service, pathFxs)
+func MakeEndpoint(ctx context.Context, cfg *config.Config, log *log.Logger, s *svc.Service) (*Endpoint, error) {
+	wep, err := web.MakeEndpoint(ctx, cfg, log, pathFxs)
 	if err != nil {
 		return nil, err
 	}
 
 	return &Endpoint{
-		wep,
+		Endpoint: wep,
+		service:  s,
 	}, nil
 }
